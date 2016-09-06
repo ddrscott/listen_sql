3630985dca47d61f926ae94e4dc4cb0e4e8db2aa
Scott Pierce <scott.pierce@centro.net>
2016-09-06 04:15:10 -0500

    Fix load issues in rbenv

--- lib/listen_sql.rb
+++ lib/listen_sql.rb
@@ -1,5 +1,22 @@
-require "listen_sql/version"
+require 'listen_sql/version'
+
+require 'shellwords'
+require 'coderay'
 
 module ListenSql
-  # Your code goes here...
+  module_function
+
+  def exec_sql(db_name, file, color: true)
+    puts "File: #{file}"
+    puts "Time: #{Time.now}"
+
+    # Show original SQL
+    puts "SQL: "
+    sql = File.read(file)
+    puts(color ? CodeRay.scan(sql, :sql).terminal : sql)
+
+    # Show Resultset
+    puts "Rows: "
+    puts `psql #{db_name} < #{Shellwords.escape file}`
+  end
 end
